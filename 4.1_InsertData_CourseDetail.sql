ec038c6465d11882cfa3fae692bf0c39c70d8391
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:35:36 -0400

    Create 4.1_InsertData_CourseDetail.sql

---
+++ 4.1_InsertData_CourseDetail.sql
@@ -0,0 +1,151 @@
+/* We use SQL code to insert data to Coursedetail Table. */
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Programming for Analytics',
+'Undergraduate',
+'This course introduces the aspects of programming that can support business analytics. The course introduces students to programming (using a language such as python) and its uses in business analytics. The course covers hands-on issues in programming for analytics which include accessing data, creating informative data graphics, writing functions, debugging, and organizing and commenting code.',
+'Prerequisites: CIS 2300 or Equivalent',
+'CIS/CS/CE',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Principles of FinTech',
+'Graduate',
+'The course is designed to give students a general understanding of
+Financial Technology (FinTech) in the context of the capital markets and
+financial services industry. The students will learn how IT affects the
+financial services industry and how to assess the potential for strategic
+advantage based on information technology. The course will survey
+emerging issues in the FinTech arena such as cryptocurrencies, blockchains,
+cybersecurity and machine learning and assess their application to an array
+of financial services.',
+'N/A',
+'CIS/FIN/BUS/ECO',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Data Mining for Business Analytics',
+'Graduate',
+'Data Mining is the process by which useful information is extracted from large amounts of data. This course is designed to provide students with the necessary tools and techniques to perform data mining and business analytics. The topics will include  essentials of data management, data preparation, and model development. In addition, students will learn about model assessment and validation. Emphasis will be placed on careful presentation of quantitative aspects of data mining and business analytics, as well as on applications to big data.',
+'Prerequisites: STA 9708',
+'CIS/BA',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Corporate Finance',
+'Undergraduate',
+'This course introduces students to the fundamental financial issues of the corporation. It covers basic concepts of debt and equity sources of financing and valuation; capital budgeting methods; cash flow forecasting and risk analysis; and the cost of capital. It introduces students to the process of securities issuance and techniques of financial planning and forecasting.',
+'Prerequisite:  FIN 300',
+'FIN/ACC/BUS/ECO',
+'Economics and Finance',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Futures and Forward Markets',
+'Graduate',
+'
+Study of derivative securities: interest, foreign currency, and equity swaps; the spot and futures markets; caps, floors, collars, and corridors; forward rate agreements (FRAs), and program trading. Market structure and valuation methods are examined.
+',
+'Pre/Co-requisite: FIN 9783 or FIN 9773',
+'FIN/ACC/BUS/ECO',
+'Economics and Finance',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Investment Analysis',
+'Undergraduate',
+'
+This course introduces students to the fundamental principles and theories of financial asset pricing and valuation. It provides students with a rigorous analysis of modern portfolio theory, the capital asset pricing model, and the valuation of common stocks and bonds. It includes an introduction to the main financial markets, their organization and functional characteristics.
+',
+'Pre/Co-requisite: FIN 3000',
+'FIN/ACC/BUS/ECO',
+'Economics and Finance',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Cost Accounting',
+'Undergraduate',
+'This course examines the measurement of costs, the compilation of cost data, and the impact of accounting data on the allocation of resources within an organization. Topics discussed include systems for cost accumulation, joint and by-products, budgeting, standard costs, and direct costing. The course integrates materials from accounting with economic analysis, quantitative methods, and behavioral science as the course also covers capital budgeting, cost-volume-profit analysis, profit performance, regression analysis, and linear programming. Credit will not be granted for both ACC 2203 and ACC 3200.',
+'N/A',
+'ACC',
+'Accountancy',
+4.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Auditing and Accounting Information Systems',
+'Graduate',
+'This course covers in-depth audits of accounting information systems and their internal
+controls. Common accounting and auditing problems encountered in computer-based information systems
+are identified and analyzed along with their solutions or other means of managing their effects. There is also
+consideration of ways of using the computer as an audit tool.',
+'Prerequisite: ACC 9110 or 9112.',
+'ACC/CIS/ECO/FIN',
+'Accountancy',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Data Analytics in Accounting',
+'Graduate',
+'Data Analytics in Accounting',
+'Prerequisite: ACC 9112 or ACC 9110 Corequisites:  ACC  9811 and  ACC  9818',
+'ACC/CIS/ECO/BA',
+'Accountancy',
+4.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Business Statistics I',
+'Undergraduate',
+'A one-semester broad-based introductory business statistics course that focuses on descriptive statistics, control charts, regression, and inferential statistics. Topics covered include graphical methods, descriptive statistics with exploratory data analysis, an introduction to control charts (with a focus on special cause and common cause variation), linear regression and correlation, the normal distribution and sampling distribution of the mean, estimation for means and proportions, and hypothesis testing for one and two groups. Students will use a microcomputer statistical package for analyzing selected data sets. This course is required for all BBA students.',
+'Prerequisite:  Sophomore status, MTH 2003 or MTH 2009 or equivalent. Prerequisite or Corequisite: CIS 2200. Not open to students who have completed STA 2100.',
+'STA',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Managerial Statistics',
+'Graduate',
+'This course provides MBA students with the statistical tools and concepts needed in business applications. Students will learn quantitative analysis critical for business decision making. Emphasis will be placed on understanding data analysis and interpretation. Topics include: interpretation of descriptive measures; applications of probability and the normal distribution; confidence interval estimation; hypothesis testing; simple linear regression models; and multiple regression models. The students will use standard spreadsheet software to work with data and apply the concepts learned. Discussions of ethical issues are integrated throughout the course. This course will enhance skills in critical thinking, as well as oral and written communication. Techniques learned in this course can be immediately put to use by the student.',
+'Not open to students who have completed FIN 9762.',
+'STA/CIS/BA/MBA/ACC/FIN/ECO',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Introduction to SAS Programming',
+'Undergraduate',
+'This course is a hands-on introduction to modern computer-intensive statistical methods, such as the bootstrap and Monte Carlo methods. It will be taught on platforms, such as Microsoft Excel and Visual Basic for Applications (VBA).  Emphasis will be placed on fundamental programming rather than the use of canned routines. Applications will be taken from the fields of finance and accounting.',
+'PREREQUISITE: STA 2000 or ECO 4000',
+'STA/CIS/BA/MBA/ACC/FIN/ECO',
+'Computer Information Systems',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Marketing Research',
+'Undergraduate',
+'Training in the basic techniques of research in marketing, including problems definition, research design, questionnaire construction, sampling, and data collection and analysis, and report preparation. The student will design and will analyze cases based on real-world business problems and provide a written report for each.',
+'Prerequisite: MKT 3000 AND STA 2000',
+'MKT',
+'Marketing and Intl Business',
+4.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Media Planning',
+'Undergraduate',
+'This course examines the development of effective media strategy for advertising and other areas of marketing communication. Each of the major media channels for promotion are covered in depth. Students develop their own media plans and conduct a variety of computer-assisted quantitative analyses to assess competitive spending, set objectives, and evaluate the audience delivery of alternative media schedules.',
+'Prerequisite: MKT 3000 AND MKT 3520',
+'MKT',
+'Marketing and Intl Business',
+3.00);
+
+INSERT INTO CourseDetail  (CourseName, Degree, Subject_information, Prerequisite, Major_requirement, Department, Units)
+VALUES ('Data-driven Marketing Strategy',
+'Graduate',
+'This course will focus on developing marketing mix and resource allocation decisions driven by quantitative analysis. The course will specifically focus on building quantitative models of pricing, channels of distribution, branding, and promotions. Topics covered include market response models, conjoint analysis techniques, resource allocation models, forecasting models, customer profitability analysis, value pricing, product line decisions, and other significant strategic marketing issues facing todays managers.',
+'Prerequisite: MKT 9703',
+'MKT/BA',
+'Marketing and Intl Business',
+3.00);
+
