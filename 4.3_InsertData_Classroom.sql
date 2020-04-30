4e08d62343c0506a7c2f30e74f4381c397b6be83
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:37:32 -0400

    Create 4.3_InsertData_Classroom.sql

---
+++ 4.3_InsertData_Classroom.sql
@@ -0,0 +1,65 @@
+/* We use SQL code to insert data to Coursedetail Table. */
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('A3150', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('A3151', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('A3152', 'Lab', 30);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('A3153', 'Regular', 100);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('B7150', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('B7151', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('B7152', 'Lab', 30);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('B7153', 'Regular', 100);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('C6250', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('C6251', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('C6252', 'Lab', 30);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('C6253', 'Regular', 100);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('D8250', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('D8251', 'Regular', 50);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('D8252', 'Lab', 30);
+
+INSERT INTO Classroom
+    (ClassroomID, Classroom_Type, MaxCapacity)
+    VALUES('D8253', 'Regular', 100);
