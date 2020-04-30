6b2aefba8671f1d4079a8a7eccf4d7c0765ed514
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:38:24 -0400

    Create 4.4_InsertData_Transcript.sql

---
+++ 4.4_InsertData_Transcript.sql
@@ -0,0 +1,213 @@
+/* We use SQL code to insert data to Transcript Table. */
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1001', '3.3', '30');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1002', '3.6', '12');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1003', '3.5', '22');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1004', '3.7', '34');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1005', '3.1', '45');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1006', '2.8', '23');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1007', '2.3', '18');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1008', '3.3', '15');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1009', '3.9', '14');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1010', '2.1', '29');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1011', '3.4', '50');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1012', '3.5', '33');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1013', '3.9', '28');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1014', '4.0', '36');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1015', '3.3', '33');
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1016', '3.8', '37');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1017', '3.6', '35');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1018', '3.7', '34');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1019', '2.5', '25');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1020', '2.9', '28');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1021', '3.3', '40');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1022', '3.5', '44');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1023', '3.3', '39');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1024', '2.9', '44');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1025', '4.0', '28');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1026', '3.8', '46');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1027', '3.3', '38');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1028', '3.7', '46');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1029', '3.2', '36');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1030', '3.6', '9');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1031', '3.9', '15');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1032', '3.7', '20');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1033', '3.9', '18');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1034', '2.6', '40');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1035', '2.9', '38');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1036', '3.0', '36');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1037', '3.0', '35');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1038', '3.7', '28');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1039', '4.0', '22');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1040', '3.6', '12');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1041', '3.6', '9');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1042', '3.7', '12');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1043', '3.2', '18');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1044', '3.3', '26');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1045', '3.4', '38');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1046', '3.1', '39');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1047', '3.7', '40');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1048', '3.7', '38');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1049', '3.8', '47');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1050', '3.5', '45');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1051', '2.9', '50');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1052', '2.4', '38');
+
+INSERT INTO Transcript
+    (TranscriptID, GPA, TotalCredit)
+    VALUES('1053', '2.3', '46');
+
