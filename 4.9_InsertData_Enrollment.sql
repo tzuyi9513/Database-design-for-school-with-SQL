74022d7d06a6d61bfa2fb71af14b4c850db6b3ab
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:48:02 -0400

    Create 4.9_InsertData_Enrollment.sql

---
+++ 4.9_InsertData_Enrollment.sql
@@ -0,0 +1,280 @@
+/* We use SQL code to insert data to Enrollment Table. */
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('42012237', '8756', '1001'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('60189043', '9875', '1002'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('77942801', '8756', '1003'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('28505188', '8756', '1004'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('60417231', '8756', '1005'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('49436677', '9875', '1006'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('69122657', '7580', '1007'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('28076079', '7580', '1008'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('36935144', '4217', '1009'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('94600626', '4217', '1010'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('62225314', '4217', '1011'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('51327795', '7580', '1012'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47158329', '3446', '1013'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('48484110', '8855', '1014'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('82718168', '3255', '1015'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('29176473', '2021', '1016'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('72773514', '3255', '1017'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('19137998', '2021', '1018'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47158329', '3255', '1013'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('48484110', '2021', '1014'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('82718168', '3446', '1015'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('29176473', '8855', '1016'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('72773514', '3446', '1017'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('19137998', '8855', '1018'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('32322720', '7566', '1019'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('17604969', '7566', '1020'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47615957', '7566', '1021'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('32310351', '7566', '1022'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('63926611', '7566', '1023'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('43705448', '6434', '1024'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('92973915', '6434', '1025'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('22216825', '7976', '1026'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47200958', '6434', '1027'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('63744655', '7976', '1028'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('43705448', '7976', '1024'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('92973915', '7976', '1025'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47559331', '1288', '1029'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('91028171', '9754', '1030'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('36030525', '1288', '1031'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('32387051', '9754', '1032'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('96727279', '2901', '1033'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('36030525', '9754', '1031'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('32387051', '2901', '1032'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47355073', '8755', '1034'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('51156575', '9011', '1035'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('52905878', '5999', '1036'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('50492937', '8655', '1037'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('58316683', '8655', '1038'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('51156575', '8755', '1035'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('52905878', '8755', '1036'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('50492937', '9011', '1037'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('58316683', '5999', '1038'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('21819872', '7657', '1039'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('92723200', '0777', '1040'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('23883576', '7657', '1041'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('70920631', '0777', '1042'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('72061949', '0777', '1043'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('21819872', '0777', '1039'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('92723200', '7657', '1040'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('39311602', '5445', '1044'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('70423802', '5445', '1045'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('15234147', '5445', '1046'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('18610448', '5445', '1047'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('25548455', '5445', '1048'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('20430070', '988', '1049'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('53103408', '988', '1050'); 
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('13272764', '0988', '1051'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('10903178', '988', '1052'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('87733041', '988', '1053'); 
+
+INSERT INTO enrollment
+ (StudentID, SessionNo, TranscriptID)
+ VALUES('47200958', '7976', '1027'); 
