18a9018b9fb11d11609936408b108b5ee1bb4aaa
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:36:48 -0400

    Create 4.2_InsertData_Course.sql

---
+++ 4.2_InsertData_Course.sql
@@ -0,0 +1,48 @@
+/* We use SQL code to insert data to Course Table. */
+
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('CIS 3140', 'Programming for Analytics', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('CIS 9555', 'Principles of FinTech', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('CIS 9660', 'Data Mining for Business Analytics', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('FIN 3610', 'Corporate Finance', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('FIN 9782', 'Futures and Forward Markets', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('FIN 3710', 'Investment Analysis', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('ACC 3200', 'Cost Accounting', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('ACC 9818', 'Auditing and Accounting Information Systems', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('ACC 9886', 'Data Analytics in Accounting', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('STA 2000', 'Business Statistics I', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('STA 9708', 'Managerial Statistics', 'Graduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('STA 4000', 'Introduction to SAS Programming', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('MKT 3600', 'Marketing Research', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('MKT 4120', 'Media Planning', 'Undergraduate');
+
+INSERT INTO Course  (CourseID, CourseName, Degree)
+VALUES ('MKT 9740', 'Data-driven Marketing Strategy', 'Graduate');
+
