b636d58485b71eef8620a4c1d9d51f6d5bd951b8
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:42:35 -0400

    Create 4.6_InsertData_Student.sql

---
+++ 4.6_InsertData_Student.sql
@@ -0,0 +1,160 @@
+/* We use SQL code to insert data to Student Table. */
+
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('69122657','Duncan','Pate','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('42012237','Bradley','Stewart','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('47158329','Fitzgerald','Guthrie','undergraduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('32322720','Xenos','Jensen','undergraduate','MKT','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('43705448','Jasper','Melton','undergraduate','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('47355073','Hamish','Richardson','graduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('47559331','Brennan','Holden','graduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('21819872','Dolan','Cantu','graduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('39311602','Arsenio','Mclaughlin','graduate','MKT','PartTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('20430070','Paul','Welch','Phd','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('28076079','Keith','Macdonald','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('60189043','Alden','Brock','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('48484110','Moses','Beach','undergraduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('17604969','Jackson','Deleon','undergraduate','MKT','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('92973915','Knox','Stein','undergraduate','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('51156575','Gary','Gaines','graduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('91028171','Ross','Dominguez','graduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('92723200','Chester','Alvarado','graduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('70423802','Trevor','Price','graduate','MKT','PartTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('53103408','Kato','Daniel','Phd','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('36935144','Rigel','Delacruz','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('77942801','Howard','Roman','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('82718168','Damon','Charles','undergraduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('47615957','Reese','Hebert','undergraduate','MKT','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('22216825','Illana','David','undergraduate','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('52905878','Quinn','Case','graduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('36030525','Olympia','Melton','graduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('23883576','Iris','Randolph','graduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('15234147','Melissa','Lynn','graduate','MKT','PartTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('13272764','Kay','Contreras','Phd','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('94600626','Quemby','Little','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('28505188','Giselle','Dennis','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('29176473','Audra','Hogan','undergraduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('32310351','Wyoming','Goodman','undergraduate','MKT','FullTime');
+
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('47200958','Jaden','Roth','undergraduate','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('50492937','Keely','Cole','graduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('32387051','Geraldine','Herrera','graduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('70920631','Zephr','Bridges','graduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('18610448','Hadassah','Norton','graduate','MKT','PartTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('10903178','Madeson','Steele','Phd','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('62225314','Athena','Case','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('60417231','Gretchen','Mcdowell','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('72773514','Rina','Rivera','undergraduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('63926611','Madeson','Webster','undergraduate','MKT','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('63744655','Tallulah','Nixon','undergraduate','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('58316683','Carissa','Cameron','graduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('96727279','Abra','Madden','graduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('72061949','Regina','Mann','graduate','FIN','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('25548455','Quintessa','Lindsey','graduate','MKT','PartTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('87733041','Hilary','Dawson','Phd','STA','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('51327795','Flavia','Castillo','undergraduate','CIS','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('49436677','Carol','Mcknight','undergraduate','ACC','FullTime');
+ 
+INSERT INTO Student  (StudentID, StudentFirstName,StudentLastName, Degree, Major, ProgramType)
+ VALUES ('19137998','Leila','Strickland','undergraduate','FIN','FullTime');
