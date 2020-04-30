d9aff34cb8fce2ebea423b38e32c757347aa1e1d
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:39:31 -0400

    Create 4.5_InsertData_Professor.sql

---
+++ 4.5_InsertData_Professor.sql
@@ -0,0 +1,81 @@
+/* We use SQL code to insert data to Professor Table. */
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1997443771', 'Kelly', 'Barrera', 'fulltime', 90000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1698078080', 'Mari', 'Mclean', 'fulltime', 100000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1775030773', 'Carter', 'Jackson', 'fulltime', 130000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1148529568', 'Quin', 'Ballard', 'fulltime', 90000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1131774660', 'Steel', 'Mayer', 'fulltime', 125000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1574751191', 'Marsden', 'Hale', 'fulltime', 80000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1061046671', 'Stuart', 'Donaldson', 'fulltime', 85000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1904605352', 'Cadman', 'Pennington', 'parttime', 30000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1970551178', 'Farrah', 'Washington', 'parttime', 50000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1240328719', 'Yoko', 'Church', 'parttime', 30000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1735156751', 'Uriah', 'Mccall', 'fulltime', 92000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1733708699', 'Amity', 'Gordon', 'fulltime', 105000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1738826610', 'Julian', 'Parks', 'fulltime', 128000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1146203408', 'Kameko', 'Webb', 'fulltime', 150000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1477000521', 'Xanthus', 'Mckee', 'fulltime', 125000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1588901445', 'Jane', 'Beck', 'fulltime', 85000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1114667337', 'Cody', 'Holder', 'fulltime', 85000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1314842598', 'Carl', 'Lawrence', 'parttime', 30000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1680104634', 'Sacha', 'Mcclure', 'parttime', 45000);
+
+INSERT INTO professor
+   (professorid, professor_first_name, professor_last_name, professor_type, salary)
+   VALUES ('1942648288', 'Rhea', 'Lester', 'parttime', 30000);
