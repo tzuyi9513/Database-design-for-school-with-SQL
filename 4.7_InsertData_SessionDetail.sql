6775c416adc2221542d6930489fc264a8baaae0a
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:44:18 -0400

    Create 4.7_InsertData_SessionDetail.sql

---
+++ 4.7_InsertData_SessionDetail.sql
@@ -0,0 +1,85 @@
+/* We use SQL code to insert data to SessionDetail Table. */
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('TuTh','07:50 AM','10:45 AM','1114667337','C6252','CIS 9555','Spring','Closed',30,'Hybrid');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Mo','07:50 AM','10:45 AM','1131774660','B7150','FIN 3610','Spring','Closed',35,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Th','07:50 AM','10:45 AM','1146203408','D8251','STA 2000','Fall','Open',30,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Fr','11:10 AM','02:05 PM','1148529568','A3153','CIS 3140','Winter','Closed',85,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Fr','02:30 PM','05:25 PM','1240328719','C6251','MKT 3600','Fall','Closed',35,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Fr','02:30 PM','05:25 PM','1314842598','C6253','ACC 9886','Fall','Wait List',45,'Online');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Sa','02:30 PM','05:25 PM','1477000521','D8252','ACC 3200','Fall','Open',30,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Tu','02:30 PM','05:25 PM','1574751191','B7151','CIS 9660','Summer','Open',25,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Fr','05:50 PM','07:10 PM','1588901445','D8253','FIN 3710','Winter','Closed',35,'Online');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('MoWe','09:05 AM','10:20 AM','1680104634','D8250','CIS 9555','Fall','Open',30,'Hybrid');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('TuTh','07:30 PM','08:45 PM','1698078080','A3151','CIS 9660','Spring','Closed',35,'Hybrid');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Th','07:50 AM','10:45 AM','1698078080','D8253','STA 4000','Fall','Open',85,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('TuTh','11:10 AM','02:05 PM','1733708699','C6253','FIN 3710','Summer','Wait List',75,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('MoWe','09:05 AM','10:20 AM','1735156751','C6252','ACC 9818','Spring','Closed',25,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('We','11:10 AM','02:05 PM','1738826610','D8250','MKT 9740','Fall','Wait List',35,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Sa','09:05 AM','10:20 AM','1775030773','A3152','MKT 4120','Fall','Open',30,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Th','07:30 PM','08:45 PM','1904605352','B7153','CIS 3140','Winter','Closed',50,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('TuTh','11:10 AM','02:05 PM','1942648288','D8251','STA 9708','Summer','Open',33,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('Sa','07:50 AM','10:45 AM','1970551178','C6250','FIN 9782','Spring','Closed',30,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('We','11:10 AM','02:05 PM','1997443771','D8252','FIN 3610','Fall','Wait List',25,'Regular');
+
+INSERT INTO SessionDetail
+(Day_of_Week,Start_Time,End_Time,ProfessorID,ClassroomID,CourseID,Semester_term,Status,Capacity,InstructionMod)
+VALUES('MoWe','05:50 PM','07:10 PM','1997443771','A3150','ACC 9818','Fall','Open',25,'Online');
