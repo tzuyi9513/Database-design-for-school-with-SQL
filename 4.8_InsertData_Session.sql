259a9f2a94692ebcb67f7af4ff441500d06913bc
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-04-30 14:45:39 -0400

    Create 4.8_InsertData_Session.sql

---
+++ 4.8_InsertData_Session.sql
@@ -0,0 +1,47 @@
+/* We use SQL code to insert data to Session Table. */
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('8756','We','05:50 PM','07:10 PM','1061046671');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('7566','Sa','07:50 AM','10:45 AM','1114667337');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('5445','TuTh','02:30 PM','05:25 PM','1114667337');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('7976','Mo','07:50 AM','10:45 AM','1131774660');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('8855','Th','07:50 AM','10:45 AM','1146203408');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('7647','Fr','11:10 AM','02:05 PM','1148529568');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('4217','Fr','02:30 PM','05:25 PM','1240328719');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('1288','Fr','02:30 PM','05:25 PM','1314842598');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('7657','Sa','02:30 PM','05:25 PM','1477000521');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('6434','Tu ','02:30 PM','05:25 PM','1574751191');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('0988','Fr','05:50 PM','07:10 PM','1588901445');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('2901','MoWe','09:05 AM','10:20 AM','1680104634');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('8755','TuTh','07:50 AM','10:45 AM','1698078080');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('3255','Th','07:30 PM','08:45 PM','1698078080');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('5999','TuTh','11:10 AM','02:05 PM','1733708699');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('9875','MoWe','09:05 AM','10:20 AM','1735156751');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('3446','We','11:10 AM','02:05 PM','1738826610');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('7580','Sa','09:05 AM','10:20 AM','1775030773');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('9754','Th','07:30 PM','08:45 PM','1904605352');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('2021','TuTh','11:10 AM','02:05 PM','1942648288');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('8655','Sa','07:50 AM','10:45 AM','1970551178');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('9011','We','11:10 AM','02:05 PM','1997443771');
+
+INSERT INTO Session(SessionNo,Day_of_Week, Start_Time,End_Time,ProfessorID) VALUES('0777','MoWe','05:50 PM','07:10 PM','1997443771');
