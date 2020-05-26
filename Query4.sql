3b1560129e820cc2abcacdfc1727c4ef019abde9
Eva Li <53924772+tzuyi9513@users.noreply.github.com>
2020-05-25 20:36:38 -0400

    Create Query4.sql

---
+++ Query4.sql
@@ -0,0 +1,11 @@
+/*
+
+4.	The university wants to know the average GPA performance to understand the study situation of students.
+
+The Average_GPA is 3.35, which is above expectation of the university.
+
+*/
+
+
+SELECT ROUND(AVG(GPA),2) AS Average_GPA FROM Transcript;
+
